c7cc546edf728130e93c8b10dff1d4ec879fbe1d
Chalasani Sravan Chowdhary <csc.education.211124@gmail.com>
2025-01-31 10:09:30 +0530

    Add files via upload

---
+++ StringExtraction.java
@@ -0,0 +1,7 @@
+public class StringExtraction{
+    public static void main(String[] args) {
+        String OriginalString="Hell0, This is Sravan";
+        String extracting = OriginalString.substring(7,11);
+        System.out.println("Extracted Sting is "+extracting);
+    }
+}
